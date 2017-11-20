b270c614beb144b75502f150f14ed2a24f904773
Florian Kraus <fkraus@hm.edu>
2017-11-20 21:37:44 +0100

    Update clearClipboard(label) to only overwrite clip if it is the same as previously created.

--- PassButler/app/src/main/java/edu/hm/cs/ig/passbutler/data/ClipboardUtil.java
+++ PassButler/app/src/main/java/edu/hm/cs/ig/passbutler/data/ClipboardUtil.java
@@ -24,6 +24,12 @@ public class ClipboardUtil {
         this.clipboardManager = (ClipboardManager) mContext.getSystemService(Context.CLIPBOARD_SERVICE);
     }
 
+    /**
+     * Copies the text to the clipboard and deletes the clip after given amount of time.
+     * @param label the label for the clip
+     * @param text  the text to copy to clipboard
+     * @param seconds   the amount of time in seconds before the clip gets deleted
+     */
     public void copyAndDelete(final String label, String text, int seconds) {
         Handler handler = new Handler(Looper.getMainLooper());
 
@@ -31,18 +37,17 @@ public class ClipboardUtil {
         ClipData clip = ClipData.newPlainText(label, text);
         // TODO: handle possible NullPointerException
         clipboardManager.setPrimaryClip(clip);
+        Log.d(TAG, clip.getDescription().getLabel().toString());
 
-        Log.d(TAG, "Clipboard1: " + clipboardManager.getPrimaryClipDescription().getLabel().toString());
-        Log.d(TAG, "Clipboard2: " + clipboardManager.getPrimaryClipDescription().getLabel().toString());
         // Overwrite clipboard after given time
         handler.postDelayed(new Runnable() {
+
             @Override
             public void run() {
                     clearClipboard(label);
-                    Toast.makeText(mContext, "Clipboard has been cleared!", Toast.LENGTH_LONG).show();
-                //}
             }
         }, seconds * 1000);
+
     }
 
     private void clearClipboard() {
@@ -54,13 +59,15 @@ public class ClipboardUtil {
 
     private void clearClipboard(String label) {
 
+        String clipboardLabel = clipboardManager.getPrimaryClip().getDescription().getLabel().toString();
         Log.d(TAG, "Got Label:" + label);
-        Log.d(TAG, "Clipboardlabel: " + clipboardManager.getPrimaryClip().toString());
-        if(clipboardManager.getPrimaryClipDescription().getLabel().toString().equals(label)) {
+        Log.d(TAG, "Clipboardlabel: " + clipboardLabel);
+        if(clipboardLabel.equals(label)) {
             Log.d(TAG, "Deleting Clipboard!");
             ClipData clip = ClipData.newPlainText("", "");
             // TODO: handle possible NullPointerException
             clipboardManager.setPrimaryClip(clip);
+            Toast.makeText(mContext, "Clipboard has been cleared!", Toast.LENGTH_LONG).show();
         }
 
     }
