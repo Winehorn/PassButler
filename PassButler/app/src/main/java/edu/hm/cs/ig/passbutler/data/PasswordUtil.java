282cf51b30cd405339dfd8b521fd89e6efa455a5
Florian Kraus <fkraus@hm.edu>
2017-11-28 11:52:59 +0100

    Add zxcvbn password check.

---
+++ PassButler/app/src/main/java/edu/hm/cs/ig/passbutler/data/PasswordUtil.java
@@ -0,0 +1,16 @@
+package edu.hm.cs.ig.passbutler.data;
+
+import com.nulabinc.zxcvbn.Strength;
+import com.nulabinc.zxcvbn.Zxcvbn;
+
+/**
+ * Created by Florian Kraus on 28.11.2017.
+ */
+
+public class PasswordUtil {
+    public static int checkPassword(String password) {
+        Zxcvbn zxcvbn = new Zxcvbn();
+        Strength strength = zxcvbn.measure(password);
+        return strength.getScore();
+    }
+}
