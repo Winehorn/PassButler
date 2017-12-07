6467160ec59cd280c2311fab235f34505e53a373
Florian Kraus <fkraus@hm.edu>
2017-12-07 22:09:38 +0100

    Add NFC support.

--- PassButler/app/src/main/java/edu/hm/cs/ig/passbutler/entry/CreatePersistenceActivity.java
+++ PassButler/app/src/main/java/edu/hm/cs/ig/passbutler/entry/CreatePersistenceActivity.java
@@ -1,12 +1,28 @@
 package edu.hm.cs.ig.passbutler.entry;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
+import android.nfc.tech.NfcF;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.security.SecureRandom;
 
 import javax.security.auth.DestroyFailedException;
 
@@ -24,12 +40,62 @@ public class CreatePersistenceActivity extends AppCompatActivity {
     private EditText passwordEditText;
     private EditText repeatPasswordEditText;
 
+    private byte[] nfcKey = new byte[128];
+
+    private boolean nfcAvailable = false;
+    private boolean nfcKeySet = false;
+    private NfcAdapter mNfcAdapter;
+    private PendingIntent mPendingIntent;
+    private IntentFilter[] mFilters;
+    private String[][] mTechLists;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_persistence);
         passwordEditText = (EditText) findViewById(R.id.password_edit_text);
         repeatPasswordEditText = (EditText) findViewById(R.id.repeat_password_edit_text);
+
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(nfcKey);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (mNfcAdapter == null) {
+            // TODO: nfcAvailable nutzen um Persistencezeug ohne NFC zu machen
+            // TODO: Strings durch Resourcestrings ersetzen
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+        } else {
+            nfcAvailable = true;
+            if (!mNfcAdapter.isEnabled()) {
+                Toast.makeText(this, "Please activate NFC.", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        // Create a generic PendingIntent that will be deliver to this activity. The NFC stack
+        // will fill in the intent with the details of the discovered tag before delivering to
+        // this activity.
+        mPendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+                0);
+
+        // Setup an intent filter for text/plain based dispatches
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try {
+            ndef.addDataType("*/*");
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("fail", e);
+        }
+
+        //IntentFilter tech = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        IntentFilter tag = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+
+        mFilters = new IntentFilter[] {
+                ndef, tag,
+        };
+
+        // Setup a tech list for all NfcF tags
+        mTechLists = new String[][] { new String[] { NfcF.class.getName() } };
     }
 
     public void createButtonOnClick(View view) {
@@ -55,7 +121,15 @@ public class CreatePersistenceActivity extends AppCompatActivity {
 
     private void createPersistence() {
         byte[] password = ArrayUtil.getContentAsByteArray(passwordEditText);
-        byte[] nfcKey = {7, 7, 7};    // TODO: Insert value from nfc tag here.
+
+        // If NFC is not available, use key substitute
+        if(!nfcAvailable || !nfcKeySet) {
+            nfcKey = null;
+            nfcKey = new byte[]{7, 7, 7};
+        }
+
+        Log.d(TAG, "createPersistence: nfcKey length: " + nfcKey.length);
+
         try {
             /*
              * CAUTION: The arrays from which the key is derived are cleared during key generation
@@ -75,4 +149,62 @@ public class CreatePersistenceActivity extends AppCompatActivity {
         accountListHandler.saveToInternalStorage(this, getString(R.string.accounts_file_name), KeyHolder.getInstance().getKey());
         Log.i(TAG, "New accounts file created.");
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mNfcAdapter != null) mNfcAdapter.enableForegroundDispatch(this, mPendingIntent, mFilters,
+                mTechLists);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mNfcAdapter != null) mNfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Log.i("Foreground dispatch", "Discovered tag in activity " + TAG);
+
+        // TODO: replace strings with string resources
+        String action = intent.getAction();
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+        if(action.equals(NfcAdapter.ACTION_TAG_DISCOVERED)) {
+            NdefFormatable ndefFormatable = NdefFormatable.get(tag);
+            if(ndefFormatable != null) {
+                try {
+                    ndefFormatable.connect();
+                    NdefRecord mimeRecord = NdefRecord.createMime(getString(R.string.nfc_app_mime_type), nfcKey);
+                    NdefMessage ndefMessage = new NdefMessage(mimeRecord);
+                    ndefFormatable.format(ndefMessage);
+                    ndefFormatable.close();
+                    nfcKeySet = true;
+                    Toast.makeText(this, "Write success!", Toast.LENGTH_LONG).show();
+                } catch (IOException | FormatException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Write failed!", Toast.LENGTH_LONG).show();
+                }
+            }
+        } else if(action.equals(NfcAdapter.ACTION_NDEF_DISCOVERED)) {
+            Ndef ndef = Ndef.get(tag);
+            if(ndef != null) {
+                try {
+                    ndef.connect();
+                    NdefRecord mimeRecord = NdefRecord.createMime(getString(R.string.nfc_app_mime_type), nfcKey);
+                    NdefMessage ndefMessage = new NdefMessage(mimeRecord);
+                    ndef.writeNdefMessage(ndefMessage);
+                    ndef.close();
+                    nfcKeySet = true;
+                    Toast.makeText(this, "Write success!", Toast.LENGTH_LONG).show();
+                } catch (IOException | FormatException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Write failed!", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
 }
+
--- PassButler/app/src/main/java/edu/hm/cs/ig/passbutler/entry/UnlockActivity.java
+++ PassButler/app/src/main/java/edu/hm/cs/ig/passbutler/entry/UnlockActivity.java
@@ -1,6 +1,16 @@
 package edu.hm.cs.ig.passbutler.entry;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
+import android.nfc.tech.NfcF;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -31,11 +41,58 @@ public class UnlockActivity extends AppCompatActivity {
     private static final String TAG = UnlockActivity.class.getName();
     private EditText passwordEditText;
 
+    private byte[] nfcKey = new byte[128];
+
+    private boolean nfcAvailable = false;
+    private boolean nfcKeyRead = false;
+    private NfcAdapter mNfcAdapter;
+    private PendingIntent mPendingIntent;
+    private IntentFilter[] mFilters;
+    private String[][] mTechLists;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unlock);
         passwordEditText = (EditText) findViewById(R.id.password_edit_text);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (mNfcAdapter == null) {
+            // TODO: nfcAvailable nutzen um Persistencezeug ohne NFC zu machen
+            // TODO: Strings durch Resourcestrings ersetzen
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+        } else {
+            nfcAvailable = true;
+            if (!mNfcAdapter.isEnabled()) {
+                Toast.makeText(this, "Please activate NFC.", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        // Create a generic PendingIntent that will be deliver to this activity. The NFC stack
+        // will fill in the intent with the details of the discovered tag before delivering to
+        // this activity.
+        mPendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+                0);
+
+        // Setup an intent filter for text/plain based dispatches
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try {
+            ndef.addDataType("*/*");
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("fail", e);
+        }
+
+        //IntentFilter tech = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        IntentFilter tag = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+
+        mFilters = new IntentFilter[] {
+                ndef, tag,
+        };
+
+        // Setup a tech list for all NfcF tags
+        mTechLists = new String[][] { new String[] { NfcF.class.getName() } };
     }
 
     public void unlockButtonOnClick(View view) {
@@ -69,7 +126,13 @@ public class UnlockActivity extends AppCompatActivity {
 
     private boolean isPasswordCorrect() throws DestroyFailedException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, JSONException, FileNotFoundException, IOException {
         byte[] password = ArrayUtil.getContentAsByteArray(passwordEditText);
-        byte[] nfcKey = {7, 7, 7};    // TODO: Insert value from nfc tag here.
+
+        // If NFC is not available, use key substitute
+        if(!nfcAvailable || !nfcKeyRead) {
+            nfcKey = null;
+            nfcKey = new byte[]{7, 7, 7};
+        }
+
         try {
             /*
              * CAUTION: The arrays from which the key is derived are cleared during key generation
@@ -81,14 +144,64 @@ public class UnlockActivity extends AppCompatActivity {
                     password,
                     nfcKey));
             AccountListHandler.getFromFile(this, getString(R.string.accounts_file_name), KeyHolder.getInstance().getKey());
+            nfcKeyRead = false;
             return true;
         }
         catch (IOException e) {
             if(e.getCause() instanceof BadPaddingException) {
                 Log.i(TAG, "I/O error was caused by bad padding.");
+                nfcKeyRead = false;
                 return false;
             }
             throw e;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mNfcAdapter != null) mNfcAdapter.enableForegroundDispatch(this, mPendingIntent, mFilters,
+                mTechLists);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mNfcAdapter != null) mNfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Log.i("Foreground dispatch", "Discovered tag in activity " + TAG);
+
+        if(intent.getAction().equals(NfcAdapter.ACTION_NDEF_DISCOVERED)) {
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            if (tag != null) {
+                Ndef ndef = Ndef.get(tag);
+                if (ndef != null) {
+                    try {
+                        ndef.connect();
+                        NdefMessage ndefMessage = ndef.getNdefMessage();
+                        Log.i(TAG, "onNewIntent: MIME type: " + ndefMessage.getRecords()[0].toMimeType());
+                        if(ndefMessage.getRecords()[0].toMimeType().equals(getString(R.string.nfc_app_mime_type))) {
+                            nfcKey = ndefMessage.getRecords()[0].getPayload();
+                            nfcKeyRead = true;
+                            Toast.makeText(this, "Read success!", Toast.LENGTH_LONG).show();
+                        } else {
+                            throw new IOException("Wrong MIME type: " + ndefMessage.getRecords()[0].toMimeType());
+                        }
+                        ndef.close();
+
+                    } catch (IOException | FormatException e) {
+                        e.printStackTrace();
+                        Toast.makeText(this, "This is the wrong tag or it's corrupted!", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+
+        } else {
+            Toast.makeText(this, "This is the wrong tag or it's corrupted!", Toast.LENGTH_LONG).show();
         }
     }
 }
