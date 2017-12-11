bde679a4ab769d91f3033e99b184d0a45b77399d
Florian Kraus <fkraus@hm.edu>
2017-12-11 16:03:37 +0100

    Move generatePassword to PasswordUtil and add generatePassword for default settings (non-expert mode).

--- PassButler/app/src/main/java/edu/hm/cs/ig/passbutler/util/PasswordUtil.java
+++ PassButler/app/src/main/java/edu/hm/cs/ig/passbutler/util/PasswordUtil.java
@@ -1,7 +1,15 @@
 package edu.hm.cs.ig.passbutler.util;
 
+import android.content.Context;
+
 import com.nulabinc.zxcvbn.Strength;
 import com.nulabinc.zxcvbn.Zxcvbn;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.hm.cs.ig.passbutler.R;
 
 /**
  * Created by Florian Kraus on 28.11.2017.
@@ -12,5 +20,70 @@ public class PasswordUtil {
         Zxcvbn zxcvbn = new Zxcvbn();
         Strength strength = zxcvbn.measure(password);
         return strength.getScore();
+    }
+
+    public static String generatePassword(Context context) {
+        final String lower = context.getString(R.string.password_generator_chars_lowercase);
+        final String upper = context.getString(R.string.password_generator_chars_uppercase);
+        final String numbers = context.getString(R.string.password_generator_chars_numbers);
+        final String special = context.getString(R.string.password_generator_chars_special);
+
+        final int length = context.getResources().getInteger(R.integer.password_generator_standard_length);
+
+        StringBuilder passwordBuilder = new StringBuilder(length);
+        SecureRandom random = new SecureRandom();
+
+        List<String> charCategories = new ArrayList<>(4);
+        charCategories.add(lower);
+        charCategories.add(upper);
+        charCategories.add(numbers);
+        charCategories.add(special);
+
+        for (int i = 0; i < length; i++) {
+            String charCategory = charCategories.get(random.nextInt(charCategories.size()));
+            int position = random.nextInt(charCategory.length());
+            passwordBuilder.append(charCategory.charAt(position));
+        }
+
+        return new String(passwordBuilder);
+    }
+
+    public static String generatePassword(boolean useLower, boolean useUpper,
+                                          boolean useNumbers, boolean useSpecial,
+                                          int length, Context context) {
+        final String lower = context.getString(R.string.password_generator_chars_lowercase);
+        final String upper = context.getString(R.string.password_generator_chars_uppercase);
+        final String numbers = context.getString(R.string.password_generator_chars_numbers);
+        final String special = context.getString((R.string.password_generator_chars_special));
+
+
+        StringBuilder passwordBuilder = new StringBuilder(length);
+        SecureRandom random = new SecureRandom();
+
+        List<String> charCategories = new ArrayList<>(4);
+        if (useLower) {
+            charCategories.add(lower);
+        }
+        if (useUpper) {
+            charCategories.add(upper);
+        }
+        if (useNumbers) {
+            charCategories.add(numbers);
+        }
+        if (useSpecial) {
+            charCategories.add(special);
+        }
+
+        if (!charCategories.isEmpty()) {
+            for (int i = 0; i < length; i++) {
+                String charCategory = charCategories.get(random.nextInt(charCategories.size()));
+                int position = random.nextInt(charCategory.length());
+                passwordBuilder.append(charCategory.charAt(position));
+            }
+        } else {
+            return "";
+        }
+
+        return new String(passwordBuilder);
     }
 }
