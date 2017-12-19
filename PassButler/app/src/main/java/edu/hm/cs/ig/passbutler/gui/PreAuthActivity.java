e4f72abae18c59d281947ec0e6283f07693b5796
Florian Kraus <fkraus@hm.edu>
2017-12-19 13:49:13 +0100

    Add onCreate to PreAuthActivity

--- PassButler/app/src/main/java/edu/hm/cs/ig/passbutler/gui/PreAuthActivity.java
+++ PassButler/app/src/main/java/edu/hm/cs/ig/passbutler/gui/PreAuthActivity.java
@@ -1,9 +1,15 @@
 package edu.hm.cs.ig.passbutler.gui;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by dennis on 16.12.17.
  */
 
 public class PreAuthActivity extends PassButlerBaseActivity {
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
