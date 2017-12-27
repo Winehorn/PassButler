ff6fecec96506e88c4a782808a8ca9d3f0784fc1
Florian Kraus <fkraus@hm.edu>
2017-12-27 16:32:26 +0100

    Change the way app checks storage permissions.

--- PassButler/app/src/main/java/edu/hm/cs/ig/passbutler/util/FileUtil.java
+++ PassButler/app/src/main/java/edu/hm/cs/ig/passbutler/util/FileUtil.java
@@ -323,20 +323,11 @@ public class FileUtil {
 
     /* Checks if external storage is available for read and write */
     public static boolean isExternalStorageWritable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
+        return Environment.getExternalStorageDirectory().canWrite();
     }
 
     /* Checks if external storage is available to at least read */
     public static boolean isExternalStorageReadable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state) ||
-                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-            return true;
-        }
-        return false;
+        return Environment.getExternalStorageDirectory().canRead();
     }
 }
